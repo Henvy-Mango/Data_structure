1f87c3a41bbb0d82e844e79f65d2005a6789b839
Mango <33375791+Henvy-Mango@users.noreply.github.com>
2018-10-05 23:32:06 +0800

    day 1

--- 01-复杂度1 最大子列和问题/源.cpp
+++ 01-复杂度1 最大子列和问题/源.cpp
@@ -7,6 +7,20 @@ int main()
 	for(int i=0;i<N;i++)
 	scanf("%d", &num[i]);
 
+	//在线更新
+	int Max=0, Now=0;
+	for (int j = 0; j < N; j++)
+	{
+		Now += num[j];
 
+		if (Now > Max)	//记录当前最大子列和
+			Max = Now;
+
+		if (Now < 0)	//当前子列和小于0，则直接抛弃重新计算
+			Now = 0;
+	}
+	
+	printf("%d", Max);
+	
 	return 0;
 }
