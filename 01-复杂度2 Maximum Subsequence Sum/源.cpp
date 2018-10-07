5be346cc306b3c95f53b6045f5222fa4178bf7f9
Mango <33375791+Henvy-Mango@users.noreply.github.com>
2018-10-07 22:35:20 +0800

    day 3

--- 01-复杂度2 Maximum Subsequence Sum/源.cpp
+++ 01-复杂度2 Maximum Subsequence Sum/源.cpp
@@ -3,44 +3,40 @@
 int main()
 {
 	int num[100005], N;
-	int head = 0, rear = 0, index = 0;
+	int head = 0, rear = 0,head_index=0;
 	scanf("%d", &N);
 	for (int i = 0; i < N; i++)
 		scanf("%d", &num[i]);
 
 	//在线更新改造
-	int Max = 0, Now = 0,flag=0;
+	int Max = 0, Now = 0, tag = 0;
 	for (int j = 0; j < N; j++)
 	{
 		if (num[j] >= 0)
-			flag = 1;
+			tag = 1;	//全为负数时标记
+
 		Now += num[j];
-		if (Now > Max)	//记录当前最大子列和
+
+		if (Now > Max|| (Now == Max && Max== 0))	//记录当前最大子列和
 		{
-			index++;
+
 			Max = Now;
-			rear = num[j];
-			if (index == 1)
-				head = num[j];
+			rear = j;
+			head = head_index;
+
 		}
-		if (Now == Max&&num[j]==0)	//记录当前最大子列和
-		{
-			index++;
-			Max = Now;
-			rear = num[j];
-			if (index == 1)
-				head = num[j];
-		}
+
 		if (Now < 0)	//当前子列和小于0，则直接抛弃重新计算
 		{
 			Now = 0;
-			index = 0;
+			head_index=j+1;
 		}
+
 	}
-	if (flag==0)
-		printf("%d %d %d", Max, num[0], num[N - 1]);
+	if (tag)
+		printf("%d %d %d", Max, num[head], num[rear]);
 	else
-		printf("%d %d %d", Max, head, rear);
+		printf("%d %d %d", Max, num[0], num[N - 1]);
 
 	return 0;
 }
