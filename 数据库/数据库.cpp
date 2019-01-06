90421f80c31715c82784b0ddc919246f1df8e1bf
梁铭轩 <807082193@qq.com>
2019-01-06 17:54:16 +0800

    update

--- 数据库/数据库.cpp
+++ 数据库/数据库.cpp
@@ -1,10 +1,12 @@
 #include "数据库.hpp"
+
 int main()
 {
 
 	//口令系统
 	int Password = 666;
-	printf("输入正确口令来继续操作:\n");
+	printf("输入正确口令来继续操作\n");
+	printf("正确口令为666\n");
 	for (int i = 0; i < 3; i++)
 	{
 		int temp;
--- 数据库/数据库.hpp
+++ 数据库/数据库.hpp
@@ -17,11 +17,12 @@ struct student {
 typedef student *Std;
 
 //全局变量
-struct student Student[50];
+struct student Student[100];
 int N = 0;
-int flag = 0;
+int Avg_flag = 0;
+int Sum_flag = 0;
 
-void Addition(student Student[], int i)
+void Addition(Std Student, int i)
 {
 	printf("请输入学生的学号:\n");
 	scanf("%d", &Student->Num);
@@ -55,7 +56,8 @@ void Add()
 		printf("正在添加第 %d 条记录\n", i + 1);
 		Addition(&Student[i], i);
 	}
-	flag = 0;
+	Avg_flag = 0;
+	Sum_flag = 0;
 
 	printf("\n");
 	printf("添加结束\n");
@@ -82,7 +84,8 @@ void Del()
 	}
 
 	N -= temp;
-	flag = 0;
+	Avg_flag = 0;
+	Sum_flag = 0;
 
 	printf("\n");
 	printf("删除结束\n");
@@ -110,7 +113,8 @@ void Modify()
 	}
 
 	Addition(&Student[temp - 1], temp);
-	flag = 0;
+	Avg_flag = 0;
+	Sum_flag = 0;
 
 	printf("\n");
 	printf("修改结束\n");
@@ -119,9 +123,9 @@ void Modify()
 
 void Display(student L)
 {
-	printf("学号:  %d  名字:  %s  年级:  %d  专业:  %s\n", L.Num,L.name,L.grade,L.major);
+	printf("学号:  %d  名字:  %s  年级:  %d  专业:  %s\n", L.Num, L.name, L.grade, L.major);
 	printf("成绩 1:  %d  成绩 2:  %d  成绩 3:  %d\n", L.score_1, L.score_2, L.score_3);
-	printf("平均成绩:  %d  总成绩:  %d\n", L.score_Avg,L.score_Sum);
+	printf("平均成绩:  %d  总成绩:  %d\n", L.score_Avg, L.score_Sum);
 	printf("\n");
 }
 void Dis()
@@ -138,13 +142,14 @@ void Dis()
 		printf("正在显示第 %d 条记录\n", i + 1);
 		Display(Student[i]);
 
-		//三个分一组，分页显示
-		if ((i+1) % 3 == 0)
+		//五个分一组，分页显示
+		if ((i + 1) % 5 == 0)
 			system("pause");
 	}
+	system("pause");
 }
 
-void Sort()
+void Avg_Sort()
 {
 	for (int j = N - 1; j >= 0; j--)
 	{
@@ -152,7 +157,7 @@ void Sort()
 		struct student L;
 		for (int k = 0; k < j; k++)
 		{
-			if (Student[k].score_Sum > Student[k + 1].score_Sum)
+			if (Student[k].score_Avg < Student[k + 1].score_Avg)
 			{
 				L = Student[k];
 				Student[k] = Student[k + 1];
@@ -163,53 +168,267 @@ void Sort()
 		if (temp == 0)
 			break;
 	}
-	flag = 1;
+	Avg_flag = 1;
+	Sum_flag = 0;
+
 	printf("\n");
-	printf("排序成功!\n");
+	printf("平均成绩排序成功!\n");
 	system("pause");
+}
+
+void Sum_Sort()
+{
+	for (int j = N - 1; j >= 0; j--)
+	{
+		int temp = 0;
+		struct student L;
+		for (int k = 0; k < j; k++)
+		{
+			if (Student[k].score_Sum < Student[k + 1].score_Sum)
+			{
+				L = Student[k];
+				Student[k] = Student[k + 1];
+				Student[k + 1] = L;
+				temp = 1;
+			}
+		}
+		if (temp == 0)
+			break;
+	}
+	Avg_flag = 0;
+	Sum_flag = 1;
+
+	printf("\n");
+	printf("总成绩排序成功!\n");
+	system("pause");
+}
+
+void Save(char *data)
+{
+	FILE *fp = fopen(data, "w+");
+
+	if (fp == NULL)
+	{
+		printf("无法打开文件!\n");
+		return;
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		fprintf(fp, "学号:  %d  名字:  %s  年级:  %d  专业:  %s  成绩 1:  %d  成绩 2:  %d  成绩 3:  %d  平均成绩:  %d  总成绩:  %d\n", Student[i].Num, Student[i].name, Student[i].grade, Student[i].major, Student[i].score_1, Student[i].score_2, Student[i].score_3, Student[i].score_Avg, Student[i].score_Sum);
+	}
+	fclose(fp);
+	return;
+}
+
+void Sort()
+{
+	int temp;
+	while (1)
+	{
+		printf("---------降序排序菜单--------\n");
+		printf("--排序成功后生成相应txt文件--\n");
+		printf("1.平均成绩排序   2.总成绩排序\n");
+		printf("0.退出\n");
+		printf("-----------------------------\n");
+		scanf("%d", &temp);
+		if (temp == 1)
+		{
+			Avg_Sort();
+			char data[15] = { "Avg_List.txt" };
+			Save(data);
+			return;
+		}
+		else if (temp == 2)
+		{
+			Sum_Sort();
+			char data[15] = { "Sum_List.txt" };
+			Save(data);
+			return;
+		}
+		else if (temp == 0)
+			return;
+		else
+		{
+			printf("指令有误!请重新输入!\n");
+			system("pause");
+		}
+	}
 }
 
 void Search()
 {
-	if (flag == 0)
+	int select;
+
+	while (1)
 	{
-		printf("\n");
-		printf("当前数据尚未排序!\n");
-		system("pause");
-		return;
-	}
+		printf("-----------查找菜单----------\n");
+		printf("---------查找前请排序--------\n");
+		printf("1.查找平均成绩   2.查找总成绩\n");
+		printf("0.退出\n");
+		printf("-----------------------------\n");
 
-	printf("输入需要查找的总成绩:\n");
+		scanf("%d", &select);
 
-	int temp;
-	scanf("%d", &temp);
-
-	printf("\n");
-
-	int low, mid, high;
-	low = 0;
-	high = N - 1;
-
-	while (low <= high)
-	{
-		mid = (low + high) / 2;
-		if (temp < Student[mid].score_Sum)
-			high = mid - 1;
-		if (temp > Student[mid].score_Sum)
-			low = mid + 1;
-		else if (temp == Student[mid].score_Sum)
+		if (select == 1)
 		{
-			printf("查找成功,正在显示第 %d 条记录\n", mid + 1);
-			Display(Student[mid]);
+			if (Avg_flag == 0)
+			{
+				printf("\n");
+				printf("当前平均成绩数据尚未排序!\n");
+				printf("请排序完成后再来!\n");
+				system("pause");
+				return;
+			}
+
+			printf("输入需要查找的平均成绩:\n");
+
+			int temp;
+			scanf("%d", &temp);
+
+			printf("\n");
+
+			int low, mid, high;
+			low = 0;
+			high = N - 1;
+
+			while (low <= high)
+			{
+				mid = (low + high) / 2;
+				if (temp < Student[mid].score_Avg)
+					low = mid + 1;
+				if (temp > Student[mid].score_Avg)
+					high = mid - 1;
+				else if (temp == Student[mid].score_Avg)
+				{
+					printf("查找成功,正在显示第 %d 条记录\n", mid + 1);
+					Display(Student[mid]);
+					system("pause");
+					return;
+				}
+			}
+
+			printf("查找失败,未找到记录!\n");
 			system("pause");
 			return;
 		}
+		else if (select == 2)
+		{
+			if (Sum_flag == 0)
+			{
+				printf("\n");
+				printf("当前总成绩数据尚未排序!\n");
+				printf("请排序完成后再来!\n");
+				system("pause");
+				return;
+			}
+
+			printf("输入需要查找的总成绩:\n");
+
+			int temp;
+			scanf("%d", &temp);
+
+			printf("\n");
+
+			int low, mid, high;
+			low = 0;
+			high = N - 1;
+
+			while (low <= high)
+			{
+				mid = (low + high) / 2;
+				if (temp < Student[mid].score_Sum)
+					low = mid + 1;
+				if (temp > Student[mid].score_Sum)
+					high = mid - 1;
+				else if (temp == Student[mid].score_Sum)
+				{
+					printf("查找成功,正在显示第 %d 条记录\n", mid + 1);
+					Display(Student[mid]);
+					system("pause");
+					return;
+				}
+			}
+
+			printf("查找失败,未找到记录!\n");
+			system("pause");
+			return;
+		}
+		else
+		{
+			printf("指令有误!请重新输入!\n");
+			system("pause");
+		}
 	}
+}
 
-	printf("查找失败,未找到记录!\n");
+void Load()
+{
+	int temp;
 
-	system("pause");
-	return;
+	while (1)
+	{
+		printf("-----------排序菜单----------\n");
+		printf("1.读取平均成绩   2.读取总成绩\n");
+		printf("0.退出\n");
+		printf("-----------------------------\n");
+
+		scanf("%d", &temp);
+
+		if (temp == 1)
+		{
+			FILE *fp = fopen("Avg_List.txt", "r+");
+
+			if (fp == NULL)
+			{
+				printf("无法打开文件!\n");
+				return;
+			}
+
+			while (!feof(fp))
+			{
+				temp = fscanf(fp, "学号:  %d  名字:  %s  年级:  %d  专业:  %s  成绩 1:  %d  成绩 2:  %d  成绩 3:  %d  平均成绩:  %d  总成绩:  %d\n", &Student[N].Num, &Student[N].name, &Student[N].grade, &Student[N].major, &Student[N].score_1, &Student[N].score_2, &Student[N].score_3, &Student[N].score_Avg, &Student[N].score_Sum);
+				if (temp != 9)
+				{
+					printf("文件格式错误!\n");
+					system("pause");
+					return;
+				}
+				Student[N].score_Sum = Student[N].score_1 + Student[N].score_2 + Student[N].score_3;
+				Student[N].score_Avg = Student[N].score_Sum / 3;
+				N++;
+			}
+			fclose(fp);
+			printf("平均成绩读取成功!\n");
+			system("pause");
+			return;
+		}
+		else if (temp == 2)
+		{
+			FILE *fp = fopen("Sum_List.txt", "r+");
+
+			if (fp == NULL)
+			{
+				printf("无法打开文件!\n");
+				return;
+			}
+
+			while (!feof(fp))
+			{
+				fscanf(fp, "学号:  %d  名字:  %s  年级:  %d  专业:  %s  成绩 1:  %d  成绩 2:  %d  成绩 3:  %d  平均成绩:  %d  总成绩:  %d\n", &Student[N].Num, &Student[N].name, &Student[N].grade, &Student[N].major, &Student[N].score_1, &Student[N].score_2, &Student[N].score_3, &Student[N].score_Avg, &Student[N].score_Sum);
+				N++;
+			}
+			fclose(fp);
+			printf("总成绩读取成功!\n");
+			system("pause");
+			return;
+		}
+		else
+		{
+			printf("指令有误!请重新输入!\n");
+			system("pause");
+		}
+	}
 }
 
 void Menu()
@@ -220,7 +439,7 @@ void Menu()
 		printf("----当前系统有  %d  条记录---\n", N);
 		printf("1.添加     2.删除     3.修改 \n");
 		printf("4.显示     5.排序     6.查找 \n");
-		printf("7.保存     0.退出\n");
+		printf("7.读取     0.退出\n");
 		printf("-----------------------------\n");
 		int select;
 		printf("请输入:");
@@ -238,8 +457,8 @@ void Menu()
 			Sort();
 		else if (select == 6)
 			Search();
-		//else if (select == 7)
-		//	Save();
+		else if (select == 7)
+			Load();
 		else if (select == 0)
 			return;
 		else
